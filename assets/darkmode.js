64740c8e29c1c62e0f9c23752077b56c34d18fc5
burak atalay <burakatalay06@gmail.com>
2025-05-31 22:56:17 +0300

    dark mode button added.

---
+++ assets/darkmode.js
@@ -0,0 +1,11 @@
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem("dark-mode", document.body.classList.contains("dark-mode"));
+}
+
+// when page laoded do user action
+window.onload = function () {
+  if (localStorage.getItem("dark-mode") === "true") {
+    document.body.classList.add("dark-mode");
+  }
+};
